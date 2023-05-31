679653951ae4c98d2d2aac987a98714e980f7a24
Teodor5090 <132367425+Teodor5090@users.noreply.github.com>
2023-05-31 15:53:53 +0000

    commit

--- website/auth.py
+++ website/auth.py
@@ -27,7 +27,7 @@ def login():
         user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
-                flash('Logged in successfully!', category='success')
+               
                 login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
@@ -61,7 +61,6 @@ def sign_up():
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
-            flash('Account succesfully created', category='Success')
             return redirect(url_for('views.home'))
 
     return render_template("signup.html", user=current_user)
